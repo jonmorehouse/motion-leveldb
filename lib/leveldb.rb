0f2f44324f49cfcf277fe5a386b04eabe260c1dd
Jon Morehouse <morehousej09@gmail.com>
2014-06-22 23:12:24 -0700

    Update application

---
+++ lib/leveldb.rb
@@ -0,0 +1,8 @@
+unless defined? (Motion::Project::Config)
+  raise "This file must be required within a RubyMotion project Rakefile"
+end
+Motion::Project::App.setup do |app|
+  Dir.glob(File.join(File.dirname(__FILE__), 'leveldb/**/*.rb')).each do |filepath|
+    app.files.unshift(filepath)
+  end
+end
---
+++ lib/leveldb/api.rb
@@ -0,0 +1,25 @@
+#module Leveldb
+
+  #class Api
+
+    #def initialize(connection)
+      #@queue = Dispatch::Queue.new("com.leveldb.database")
+      #@conn = connection
+    #end
+
+    #def get(key, &cb)
+
+    #end
+
+    #def get_keys(keys, &cb)
+
+      ## create a queue, grab the keys and use async_barrier
+
+    #end
+
+    #def set(keys, value, &cb)
+
+    #end
+
+  #end
+#end
